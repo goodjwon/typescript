2bdb3dfc131583fa240cef5decd10cfbe626f1b0
goodjwon <goodjwon@gmail.com>
2021-02-01 21:29:17 +0900

    type 익히기

--- 2-TYPES/loading.ts
+++ 2-TYPES/loading.ts
@@ -1,45 +1,41 @@
 {
-    /**
-     * Print Loading State
-     */
-    type LoadingState = {
-        state: 'loading';
+  /**
+   * Print Loading State
+   */
+  type LoadingState = {
+    state: 'loading';
+  };
+
+  type SuccessState = {
+    state: 'success';
+    response: {
+      body: string;
     };
+  };
 
-    type SuccessState = {
-        state: 'success';
-        response: {
-            body: string;
-        };
-    };
+  type FailState = {
+    state: 'fail';
+    reason: string;
+  };
 
-    type FailState = {
-        state: 'fail';
-        reason: string;
-    };
+  type ResourceLoadState = LoadingState | SuccessState | FailState;
 
-    type ResourceLoadState = LoadingState | SuccessState | FailState;
+  printLoginState({ state: 'loading' }); // 👀 loading...
+  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
+  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
 
-    printLoginState({ state: 'loading' }); // 👀 loading...
-    printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-    printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
-
-
-    function printLoginState(state: ResourceLoadState) {
-
-        switch (state.state) {
-            case 'loading':
-                console.log(state.state)
-                break;
-            case 'success':
-                console.log(state.response.body)
-                break;
-            case 'fail':
-                console.log(state.reason)
-                break;
-            default:
-                throw Error(`unkwon error: ${state}`)
-        }
-
+  function printLoginState(state: ResourceLoadState){
+    switch (state.state){
+      case 'loading':
+        console.log('👀 loading...')
+        break;
+      case 'success':
+        console.log('😃 loaded')
+        break
+      case 'fail':
+        console.log('😱 no network')
+      default:
+        throw new Error('error')
     }
+  }
 }
