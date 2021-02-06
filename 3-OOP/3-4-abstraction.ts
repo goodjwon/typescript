e83d1fd8b8043ea479cfe9f067457605793a8d59
goodjwon <goodjwon@gmail.com>
2021-02-06 19:54:07 +0900

    여러 인터페이스 상속 및 구현 - 인터페이스는 다중으로 상속 가능하다. - 각 구현체는 인터페이스 종료에 따라 선언된 메소드 사용이 달리 된다.

--- 3-OOP/3-4-abstraction.ts
+++ 3-OOP/3-4-abstraction.ts
@@ -8,55 +8,58 @@
     interface CoffeeMaker {
         makeCoffee(shots: number): CoffeeCup;
     }
-    
-    class CoffeeMachine implements CoffeeMaker{
+
+    interface CommercialCoffeeMaker {
+        makeCoffee(shots: number): CoffeeCup;
+        fillCoffeeBeans(beans: number): void;
+        clean(): void;
+    }
+
+    class CoffeeMachine implements CoffeeMaker, CommercialCoffeeMaker {
         static BEANS_GRAMM_PER_SHOT = 7;    // class level
         private coffeeBeans: number = 0;    // instance level
-        
-        private constructor(coffeeBeans: number){
+
+        private constructor(coffeeBeans: number) {
             this.coffeeBeans = coffeeBeans
         }
 
-        static makeMachine(coffeeBeans: number): CoffeeMachine{
+        static makeMachine(coffeeBeans: number): CoffeeMachine {
             return new CoffeeMachine(coffeeBeans);
         }
 
-        fillCoffeeBeans(beans: number){
-            if(beans < 0){
+        fillCoffeeBeans(beans: number) {
+            if (beans < 0) {
                 throw new Error('value for beans should be greater than 0');
             }
             this.coffeeBeans += beans;
         }
 
-        /**
-         * 추상화...
-         * 커피를 만들기 위해서 많은 메소드가 필요하지만 외부에 필요한 것은 특정적임 
-         * 내부용과 외부용을 접근제어자를 통해서 구분 할 수 있음.
-         * 
-         * 
-         */
+        clean(): void {
+            console.log('cleaning the machine ..🧹🧹')
+        }
 
-        private grindBeans(shots: number){
+
+        private grindBeans(shots: number) {
             console.log(`grinding beans for ${shots}`);
-            if(this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
-                 throw new Error('Not enough coffee beans!') 
-                }
+            if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+                throw new Error('Not enough coffee beans!')
+            }
             this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
         }
 
-        private preheat(){
-            console.log('heating up...')
+        private preheat() {
+            console.log('heating up...🔥🔥')
         }
 
-        private extract(shots: number): CoffeeCup{
-            console.log(`Pulling ${shots} shots...coffee`)
+        private extract(shots: number): CoffeeCup {
+            console.log(`Pulling ${shots} shots...coffee ☕☕☕`)
             return {
                 shots,
                 hasMilk: false
             };
         }
 
-        makeCoffee(shots: number): CoffeeCup{
+        makeCoffee(shots: number): CoffeeCup {
             this.grindBeans(shots);         //커피를 필요한 샷만큼 간다.
             this.preheat();                 //예열한다.
             return this.extract(shots);     //추출한다.
@@ -64,11 +67,32 @@
         }
     }
 
-    const maker = CoffeeMachine.makeMachine(32);
+    class AmateurUser {
+        constructor(private machine: CoffeeMaker) {
 
-    maker.fillCoffeeBeans(5);
-    console.log(maker);
-    console.log(maker.makeCoffee(3));
+        }
+        makeCoffee() {
+            const coffee = this.machine.makeCoffee(2);
+            console.log(coffee);
+        }
+    }
 
+    class ProBarista {
+        constructor(private machine: CommercialCoffeeMaker) {
+
+        }
+        makeCoffee() {
+            const coffee = this.machine.makeCoffee(2);
+            console.log(coffee);
+            this.machine.fillCoffeeBeans(45)
+            this.machine.clean();
+        }
+    }
+
+    const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
+    const amateur = new AmateurUser(maker);
+    const pro = new ProBarista(maker);
+
+    pro.makeCoffee();
 
 }
