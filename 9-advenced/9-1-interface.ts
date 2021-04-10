26a7ac981a25f11dafbb2375c9f925b8c7d87b3a
goodjwon <jwon76@naver.com>
2021-04-10 19:09:25 +0900

    start chapter9

---
+++ 9-advenced/9-1-interface.ts
@@ -0,0 +1,61 @@
+{
+    type PositionType = {
+        x: number;
+        y: number;
+    };
+    
+    interface PositionInterface {
+        x: number;
+        y: number;
+    }
+    
+    // object
+    const obj1: PositionType = {
+        x: 1,
+        y: 1
+    };
+    
+    const obj2: PositionInterface = {
+        x: 1,
+        y: 1,
+        z: 1,
+    }
+
+    // class
+    class Pos1 implements PositionType {
+        x: number;
+        y: number;
+    }
+
+    class Pos2 implements PositionInterface {
+        x: number;
+        y: number;
+        z: number;
+    }
+
+    // Extends
+    interface ZPositionInterface extends PositionInterface{
+        z: number;
+    }
+
+    type ZPositionType = PositionType & {
+        z: number
+    }
+
+    // only interface can be merged.
+    interface PositionInterface{
+        z: number;
+    }
+
+    // type PositionType {} Do not uesd.
+
+    type Person = {
+        name: string,
+        age: number,
+    }
+
+    type Name = Person['name'] //string default..
+
+    type NumberType = number;
+    type Direction = 'left' | 'right'
+}
