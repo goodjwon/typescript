dc445d91f02bf646519a8e7a0dfc81c6283235a7
goodjwon <jwon76@naver.com>
2021-08-16 18:27:45 +0900

    make base component

--- 12-mm/dist/app.js
+++ 12-mm/dist/app.js
@@ -1,8 +1,11 @@
-import { PageComponent } from './componets/page.js';
+import { ImageComponent } from './componets/page/item/image.js';
+import { PageComponent } from './componets/page/page.js';
 class App {
     constructor(appRoot) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
+        const image = new ImageComponent('Image user Title', 'https://picsum.photos/600/300');
+        image.attachTo(appRoot, 'beforeend');
     }
 }
 new App(document.querySelector('.document'));
--- 12-mm/src/app.ts
+++ 12-mm/src/app.ts
@@ -1,10 +1,14 @@
-import { PageComponent } from './componets/page.js';
+import { ImageComponent } from './componets/page/item/image.js';
+import { PageComponent } from './componets/page/page.js';
 
 class App {
     private readonly page: PageComponent;
     constructor(appRoot: HTMLElement){
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
+
+        const image = new ImageComponent('Image user Title', 'https://picsum.photos/600/300');
+        image.attachTo(appRoot, 'beforeend');
     }
 }
 
